528ca80e9846bc15b3b409b1ff0f59f8d09f497f
Bernardo Galindo <bernardo466@gmail.com>
2012-03-10 18:03:17 -0600

    add routes and initial specs

--- specs/test_key.rb
+++ specs/test_key.rb
@@ -1,31 +1,29 @@
 require 'minitest/autorun'
 
-describre Key, "Describe how keys shoudl be created" do
+describe Key, "Describe how keys shoudl be created" do
   
   before do
-    Key.new
+   @key =  Key.new
   en
 
-  describre "When I want to create without name" do
+  describe "When I want to create without name" do
     it "showld have a name" do
-      key.name = ""
-      key.secret = "password"
-      Key.save.must_be false
+      @key.name = ""
+      @key.secret = "password"
+      @Key.save.must_be false
     end
  
 
     it "When I want to create one witout  key" do
-      key.name = "bank"
-      key.secret = ""
-      key.save.must_be false
+      @key.name = "bank"
+      @key.secret = ""
+      @key.save.must_be false
     end
 
     it "when I send a key with name and secret" do
-       key.name = "test"
-       key.secret = "password"
-       key.save.must_be true
+       @key.name = "test"
+       @key.secret = "password"
+       @key.save.must_be true
     end
   end
-
-
 end
